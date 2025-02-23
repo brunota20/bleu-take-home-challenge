ef02d336847b41f973689f65140d9cd7c4110f46
brunota20 <brunot.anjos@gmail.com>
2025-02-23 09:59:07 -0300

    changing wallet to rainbow

--- apps/web/src/components/connect-wallet-button.tsx
+++ apps/web/src/components/connect-wallet-button.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { ConnectKitButton } from 'connectkit';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export const ConnectWalletButton = () => {
   return (
-    <ConnectKitButton.Custom>
-      {({ show, isConnected, address }) => (
-        <Button onClick={show} type="button" className="py-2 px-4 rounded-lg">
-          {isConnected ? `${address?.slice(0, 6)}...${address?.slice(-4)}` : 'Connect Wallet'}
-        </Button>
-      )}
-    </ConnectKitButton.Custom>
+    <div className="relative z-40">
+      <ConnectButton
+      label="Sign in"
+      chainStatus="icon"
+      accountStatus={{
+        smallScreen: 'avatar',
+        largeScreen: 'full',
+      }}/>
+    </div>
   );
 };
--- apps/web/src/providers/web3-provider.tsx
+++ apps/web/src/providers/web3-provider.tsx
@@ -1,48 +1,63 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
-import type { ReactNode } from 'react';
-import { http, WagmiProvider, createConfig } from 'wagmi';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { useEffect, useState, type ReactNode } from 'react';
+import { http, WagmiProvider, Config  } from 'wagmi';
 import { arbitrum, base, mainnet, optimism, polygon, sepolia } from 'wagmi/chains';
+import '@rainbow-me/rainbowkit/styles.css'
 
-const walletConnectProjectId = '';
-
-const config = createConfig(
-  getDefaultConfig({
-    // Your dApps chains
-    chains: [
-      // mainnet, optimism, arbitrum, base, polygon,
-      sepolia,
-    ],
-    transports: {
-      // [mainnet.id]: http(process.env.NEXT_PUBLIC_MAINNET_RPC_URL ?? ""),
-      // [optimism.id]: http(process.env.NEXT_PUBLIC_OPTIMISM_RPC_URL ?? ""),
-      // [arbitrum.id]: http(process.env.NEXT_PUBLIC_ARBITRUM_RPC_URL ?? ""),
-      // [base.id]: http(process.env.NEXT_PUBLIC_BASE_RPC_URL ?? ""),
-      // [polygon.id]: http(process.env.NEXT_PUBLIC_POLYGON_RPC_URL ?? ""),
-      [sepolia.id]: http(process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL ?? ''),
-    },
-
-    // Required API Keys
-    walletConnectProjectId,
-
-    // Required App Info
-    appName: 'Next Bleu Starter',
-    // Optional App Info
-    appDescription: 'Template for web3 next projects',
-    appUrl: 'http://localhost:3000',
-    appIcon: 'https://cdn-icons-png.flaticon.com/128/4064/4064205.png',
-  })
-);
 
 const queryClient = new QueryClient();
 
 export const Web3Provider = ({ children }: { children: ReactNode }) => {
+//   const [config, setConfig] = useState<Config | null>(null);
+
+// useEffect(() => {
+//     const clientConfig = getDefaultConfig({
+//       appName: 'Next Bleu Starter',
+//       projectId: "Next Bleu Starter",
+//       appDescription: 'Template for web3 next projects',
+//       appUrl: 'http://localhost:3000',
+//       appIcon: 'https://cdn-icons-png.flaticon.com/128/4064/4064205.png',
+//       chains: [sepolia],
+//       transports: {
+//         [sepolia.id]: http(process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL),
+//       },
+//     });
+//     setConfig(clientConfig);
+//   }, []);
+
+  // if (!config) {
+  //   return <div>Loading...</div>;
+  // }
+  const config = getDefaultConfig({
+  appName: 'Next Bleu Starter',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
+  appDescription: 'Template for web3 next projects',
+  appUrl: 'http://localhost:3000',
+  appIcon: 'https://cdn-icons-png.flaticon.com/128/4064/4064205.png',
+  ssr: true,
+});
+
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider>{children}</ConnectKitProvider>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
